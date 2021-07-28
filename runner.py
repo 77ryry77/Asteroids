29de9ed23d5e9de667f515c67a16e46e1aca3cdb
unknown <ryanadalal@gmail.com>
2021-07-28 19:05:17 -0400

    Finished movement, working no gravity and accel and deccel for space ship

--- runner.py
+++ runner.py
@@ -7,22 +7,23 @@ import pygame
 
 
 pygame.init()
-window = pygame.display.set_mode((500, 500))
+window = pygame.display.set_mode((1500, 1000))
 
 
 
-a = Shape([Point(1, 0), Point(-1, -1), Point(-1, 1)])
+a = Shape([Point(1.5, 0), Point(-1.5, -1), Point(-.5, 0), Point(-1.5, 1)])
 
 while True:
     pygame.event.get()
     pygame.time.delay(10)
     window.fill((10, 10, 10))
-    a.draw(window, (255, 255, 255), 10, 250)
+    a.draw(window, (255, 255, 255), 10, 750, 500)
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        a.move(1)
+        a.move()
     elif keys[pygame.K_d]:
-        a.rotate(math.pi/180)
+        a.rotate(math.pi/90)
     elif keys[pygame.K_a]:
-        a.rotate(-math.pi/180)
+        a.rotate(-math.pi/90)
+    a.update()
     pygame.display.update()
--- shape.py
+++ shape.py
@@ -6,7 +6,9 @@ class Shape:
     def __init__(self, points):
         self.points = points
         self.x = 0
+        self.xv = 0
         self.y = 0
+        self.yv = 0
         self.angle = 0
 
     def rotate(self, angle):
@@ -18,15 +20,27 @@ class Shape:
         for point in self.points:
             point.scale(scaleFactor)
 
-    def getDrawCords(self, shift, scale):
+    def getDrawCords(self, shiftx, shifty, scale):
         result = []
         for p in self.points:
-            result.append(((p.getX()) * scale + self.x + shift, (p.getY()) * scale + self.y + shift))
+            result.append(((p.getX()) * scale + self.x + shiftx, (p.getY()) * scale + self.y + shifty))
         return result
 
-    def move(self, distance):
-        self.x += math.cos(self.angle) * distance
-        self.y += math.sin(self.angle) * distance
+    def move(self):
+        self.xv += math.cos(self.angle)/50
+        if self.xv > 2:
+            self.xv = 2
+        if self.xv < -2:
+            self.xv = -2
+        self.yv += math.sin(self.angle)/50
+        if self.yv > 2:
+            self.yv = 2
+        if self.yv < -2:
+            self.yv = -2
+    
+    def update(self):
+        self.x += self.xv
+        self.y += self.yv
 
-    def draw(self, window, color, scale, shift):
-        pygame.draw.lines(window, color, True, self.getDrawCords(shift, scale), 3)
+    def draw(self, window, color, scale, shiftx, shifty):
+        pygame.draw.lines(window, color, True, self.getDrawCords(shiftx, shifty, scale), 3)
